accd484d2ff76953afddf08c4ec6e28bca8b68e7
Adam Robillard <hello@adamrobillard.ca>
2025-04-08 16:38:56 -0400

    Basic page content and stling

---
+++ src/lib/helpers.ts
@@ -0,0 +1,32 @@
+export function applyClasses(
+  initialClass: string | string[],
+  additionalClass?: string | string[],
+): string {
+  // convert provided classes to arrays if provided as a string
+  const initialAsArray: string[] =
+    typeof initialClass === "string" ? initialClass.split(" ") : initialClass;
+  const additionalAsArray: string[] | undefined =
+    typeof additionalClass === "string"
+      ? additionalClass.split(" ")
+      : additionalClass;
+
+  // merge into one array
+  const combinedArray: string[] = additionalAsArray
+    ? initialAsArray.concat(additionalAsArray)
+    : initialAsArray;
+
+  // join array into single string with classes separated by spaces and return
+  return combinedArray.join(" ");
+}
+
+export function applyClassesConditional(
+  initialClass: string | string[],
+  additionalClass: string | string[],
+  condition: boolean | undefined,
+): string {
+  if (condition) {
+    return applyClasses(initialClass, additionalClass);
+  } else {
+    return applyClasses(initialClass);
+  }
+}
